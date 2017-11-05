a17ed739d0d26688c5af596de100976c3dbd8b00
Georg Ledermann <mail@georg-ledermann.de>
2017-11-05 10:42:55 +0100

    Refactor

--- app/javascript/bundles/App/components/PostListing.jsx
+++ app/javascript/bundles/App/components/PostListing.jsx
@@ -19,55 +19,61 @@ export default class PostListing extends React.Component {
   }
 
   fetchPosts = () => {
-    if (!this.state.eof) {
-      var queryParam = location.search.split('q=')[1];
+    var queryParam = location.search.split('q=')[1];
 
-      $.ajax({
-        method: 'GET',
-        url: this.props.url,
-        data: { page: this.state.page + 1, q: queryParam },
-      }).done((data) => {
-        if (data.length > 0) {
-          this.setState((prevState, props) => {
-            return {
-              page: prevState.page + 1,
-              posts: prevState.posts.concat(data)
-            };
-          });
-        } else {
-          this.setState({eof: true});
-        }
-      });
+    $.ajax({
+      method: 'GET',
+      url: this.props.url,
+      data: { page: this.state.page + 1, q: queryParam },
+    }).done((data) => {
+      if (data.length > 0) {
+        this.setState((prevState, props) => {
+          return {
+            page: prevState.page + 1,
+            posts: prevState.posts.concat(data)
+          };
+        });
+      } else {
+        this.setState({eof: true});
+      }
+    });
+  }
+
+  renderTable() {
+    return (
+      <table className="table">
+        <thead>
+          <tr className="thead-inverse">
+            <th>Content</th>
+            <th className="text-nowrap text-right">Last Update</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.posts.map(function(post) {
+            return <PostRow key={post.id} post={post} />
+          })}
+        </tbody>
+      </table>
+    );
+  }
+
+  renderWaypoint() {
+    if (!this.state.eof) {
+      return (
+        <Waypoint onEnter={this.fetchPosts}>
+          <div className="text-center">
+            <i className="fa fa-spinner fa-spin fa-3x"/>
+          </div>
+        </Waypoint>
+      );
     }
   }
 
   render() {
-    let spinner = null;
-    if (!this.state.eof) {
-      spinner = <div className="text-center">
-        <i className="fa fa-spinner fa-spin fa-3x"/>
-      </div>
-    }
-
     return (
       <div>
-        <table className="table">
-          <thead>
-            <tr className="thead-inverse">
-              <th>Content</th>
-              <th className="text-nowrap text-right">Last Update</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.posts.map(function(post) {
-              return <PostRow key={post.id} post={post} />
-            })}
-          </tbody>
-        </table>
-
-        <Waypoint onEnter={this.fetchPosts}>
-          {spinner}
-        </Waypoint>
+        {this.renderTable()}
+        {this.renderWaypoint()}
       </div>
     );
   }
